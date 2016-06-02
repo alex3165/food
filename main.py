fb8568bf41675df5a1df7d466f9ff36a3f63dacf
Alexandre Rieux <alexr.3165@gmail.com>
2016-06-02 09:39:11 +0100

    Improve script and fix food help command

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from random import randint
 
 FILE_PATH = os.path.expanduser('~/Development/food/data_food.json')
 
-def commands():
+def commands(data):
   for com in ALL_COMMANDS:
     print('[{0}] command {1}'.format(com['name'], com['description']))
 
@@ -65,6 +65,11 @@ def addHistory(data):
   data["history"][elTime] = sys.argv[2]
   save(data)
 
+def getHistory(data):
+  for date, identifier in data['history'].items():
+    opt = data['options'][identifier]
+    print('You eat {0} at {1} on {2}'.format(opt['type'], opt['name'], date))
+
 def save(data):
   with open(FILE_PATH, 'w') as outfile:
     json.dump(data, outfile)
@@ -80,6 +85,11 @@ ALL_COMMANDS = [{
   'description': 'Add a new food option, take 2 parameters: [place_name] [food_type]'
 },
 {
+  'name': 'history',
+  'method': getHistory,
+  'description': 'Return all the history'
+},
+{
   'name': 'rm',
   'method': removeElement,
   'description': 'Remove a food option by name'
@@ -87,7 +97,7 @@ ALL_COMMANDS = [{
 {
   'name': 'set',
   'method': addHistory,
-  'description': 'Add a value to history'
+  'description': 'Add a value to history, take 2 parameters: [option_id] [date: mm/dd/yy]'
 },
 {
   'name': 'help',
@@ -107,7 +117,7 @@ def main():
   if(len(sys.argv) > 1):
     switch(sys.argv[1])(data)
   else:
-    commands()
+    commands(data)
 
 
 def switch(opt):
